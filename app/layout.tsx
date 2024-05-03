cb5396d761affc8b9ea3fc86c1f079632119dfec
Frankpesta <franklinolisaemeka3@gmail.com>
2024-05-03 17:09:21 +0100

    bootstrapped the application

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import ThemeProvider from "@/context/ThemeProvider";
+import "../styles/prism.css";
+
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600", "700"],
+	variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+	title: "CresaRepo",
+	description:
+		"A project work built for the submission and management of project works for the departmet of Computer and Robotics Education, University of Nigeria, Nsukka",
+	icons: {
+		icon: "",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+				<ClerkProvider
+					appearance={{
+						elements: {
+							formButtonPrimary: "primary-gradient",
+							footerActionLink: "primary-text-gradient hover:text-primary-500",
+						},
+					}}>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
+	);
+}
