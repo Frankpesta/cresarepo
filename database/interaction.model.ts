208d8546ee660e1ae356308472edbf4c1ca754f1
Frankpesta <franklinolisaemeka3@gmail.com>
2024-05-06 17:57:27 +0100

    tags and actions done

---
+++ database/interaction.model.ts
@@ -0,0 +1,22 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IInteraction extends Document {
+	user: Schema.Types.ObjectId;
+	action: string;
+	project: Schema.Types.ObjectId;
+	tags: Schema.Types.ObjectId;
+	createdAt: Date;
+}
+
+const InteractionSchema = new Schema({
+	user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+	action: { type: String, required: true },
+	project: { type: Schema.Types.ObjectId, ref: "Project" },
+	tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+	createdAt: { type: Date, default: Date.now },
+});
+
+const Interaction =
+	models.Interaction || model("Interaction", InteractionSchema);
+
+export default Interaction;
---
+++ lib/actions/user.action.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import User from "@/database/user.model";
+import { connetToDatabase } from "../mongoose";
+import { CreateUserParams } from "./shared.types";
+
+export async function createUser(userData: CreateUserParams) {
+	try {
+		connetToDatabase();
+		const newUser = await User.create(userData);
+
+		return newUser;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
