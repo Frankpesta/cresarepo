5a1b2a62e5605aeb238619e06f78ef0c3679fcbf
Frankpesta <franklinolisaemeka3@gmail.com>
2024-07-08 06:12:20 +0100

    fixes

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,36 +7,13 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getTimestamp = (createdAt: Date): string => {
-	const now = new Date();
-	const timeDifference = now.getTime() - createdAt?.getTime();
+	const date = new Date(createdAt);
 
-	// Define time intervals in milliseconds
-	const minute = 60 * 1000;
-	const hour = 60 * minute;
-	const day = 24 * hour;
-	const week = 7 * day;
-	const month = 30 * day;
-	const year = 365 * day;
+	const day = String(date.getDate()).padStart(2, "0");
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	const year = date.getFullYear();
 
-	if (timeDifference < minute) {
-		const seconds = Math.floor(timeDifference / 1000);
-		return `${seconds} ${seconds === 1 ? "secomd" : "secomds"} ago`;
-	} else if (timeDifference < hour) {
-		const minutes = Math.floor(timeDifference / minute);
-		return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
-	} else if (timeDifference < day) {
-		const hours = Math.floor(timeDifference / hour);
-		return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
-	} else if (timeDifference < week) {
-		const days = Math.floor(timeDifference / week);
-		return `${days} ${days === 1 ? "day" : "days"} ago`;
-	} else if (timeDifference < month) {
-		const months = Math.floor(timeDifference / month);
-		return `${months} ${months === 1 ? "week" : "weeks"} ago`;
-	} else {
-		const years = Math.floor(timeDifference / year);
-		return `${years} ${years === 1 ? "year" : "years"} ago`;
-	}
+	return `${day}-${month}-${year}`;
 };
 
 export const formatAndDivideNumber = (num: number): string => {
